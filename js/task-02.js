c02fe27912c3e66a326fc047e1d9b6076ff84f65
VladyslavKshenin <legendary.1bk@gmail.com>
2023-09-14 18:57:12 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -10,16 +10,6 @@ const ingredients = [
 // Отримуємо доступ до списку ul#ingredients
 const ingredientsList = document.querySelector("#ingredients");
 
-// Створюємо фрагмент для оптимізації додавання багатьох елементів
-const fragment = document.createDocumentFragment();
+const ingredientsHTML = ingredients.map((ingredient) => `<li class="item">${ingredient}</li>`).join("");
 
-// Перебираємо масив ingredients та створюємо та додаємо кожен елемент в список
-ingredients.forEach((ingredient) => {
-  const li = document.createElement("li");
-  li.textContent = ingredient;
-  li.classList.add("item");
-  fragment.appendChild(li);
-});
-
-// Додаємо всі елементи разом в список ul#ingredients за одну операцію
-ingredientsList.appendChild(fragment);
+ingredientsList.innerHTML = ingredientsHTML;
